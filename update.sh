18eb3e16f8af5f98f1438ce6df82634be4c7cc85
zouzedong <38758850+zouzedong@users.noreply.github.com>
2018-05-02 13:24:36 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ echo "准备开始安装"
 read -p "回车后开始安装"
 echo "请输入你的内网ip" 
 read -p "内网ip： " ip
-cp /root/pubgradar/restart.sh /root/restart.sh
+cp /root/radar2018502/restart.sh /root/restart.sh
 chmod +x restart.sh
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
@@ -28,7 +28,7 @@ cd libpcap-1.8.1
 make
 make install
 
-git clone https://github.com/794959818/pubgradar.git
+git clone https://github.com/zouzedong/radar2018502.git
 cd pubgradar/
 npm i
 npm i -g pino
